d32aaa5ec4e9ea0769ba7fa1efbb500ec68293e7
alberto56 <albert@mediatribe.net>
2016-06-18 13:14:56 -0400

    Issue #4 on-the-fly mock objects

--- src/test/LegacyCodeExampleTest.php
+++ src/test/LegacyCodeExampleTest.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once('/testable/mycode/LegacyCodeExample.php');
-require_once('/testable/mycode/mock/LegacyCodeExampleMock.php');
 
 class LegacyCodeExampleTest extends \PHPUnit_Framework_TestCase {
 
@@ -9,7 +8,13 @@ class LegacyCodeExampleTest extends \PHPUnit_Framework_TestCase {
    * @dataProvider providerTestSequential
    */
   public function testTestSequential($start_count, $end_count, $message) {
-    $legacy_code = new LegacyCodeExampleMock();
+    $legacy_code = $this->getMockBuilder('LegacyCodeExample')
+      ->setMethods(array('drupalSetMessage', 't'))
+      ->getMock();
+    $legacy_code->method('drupalSetMessage')
+      ->will($this->returnCallback(array($this, 'drupalSetMessage')));
+    $legacy_code->method('t')
+      ->will($this->returnCallback(array($this, 't')));
 
     $dummy_data = array();
     for ($i = 0; $i < $start_count; $i++) {
@@ -20,7 +25,7 @@ class LegacyCodeExampleTest extends \PHPUnit_Framework_TestCase {
     $legacy_code->trimData($dummy_data);
     $this->assertTrue(count($dummy_data) == $end_count, count($dummy_data) . " != $end_count");
 
-    $last_message = $legacy_code->getLastMessage();
+    $last_message = $this->getLastMessage();
     $this->assertTrue($last_message == $message, "$last_message == $message");
   }
 
@@ -31,6 +36,23 @@ class LegacyCodeExampleTest extends \PHPUnit_Framework_TestCase {
       array(3, 3, 'NO MESSAGE'),
       array(0, 0, 'NO MESSAGE'),
     );
+  }
+
+  function drupalSetMessage($message) {
+    $this->lastMessage = $message;
+  }
+
+  function t($string, $args) {
+    // Reproduce t() becasue PHPUnit does not know about it.
+    $return = $string;
+    foreach ($args as $key => $value) {
+      $return = str_replace($key, $value, $return);
+    }
+    return $return;
+  }
+
+  function getLastMessage() {
+    return isset($this->lastMessage) ? $this->lastMessage : 'NO MESSAGE';
   }
 
 }
